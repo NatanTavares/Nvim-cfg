30c658087400a38310eb97b01a473913ade09f95
NatanTavares <nattantavares.s15@gmail.com>
2024-08-07 12:15:35 -0300

    Refactor plugins

--- init.lua
+++ init.lua
@@ -27,62 +27,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    -- theme
-    'olivercederborg/poimandres.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('poimandres').setup {
-        -- leave this setup function empty for default config
-        -- or refer to the configuration section
-        -- for configuration options
-      }
-    end,
-
-    -- telescope
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-
-    -- treesitter
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function () 
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-          ensure_installed = { "c", "json", "lua", "proto", "typescript", "go", "gomod", "gosum", "vim", "vimdoc", "regex", "sql" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end
-    },
-
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      }
-    }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
-  install = { colorscheme = { "poimandres" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
+require("lazy").setup("plugins")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,8 @@
-local spec = {
-  spec = {
-    -- add your plugins here
-    -- theme
+return {
+  -- add your plugins here
+
+  -- theme
+  {
     'olivercederborg/poimandres.nvim',
     lazy = false,
     priority = 1000,
@@ -12,48 +13,47 @@ local spec = {
         -- for configuration options
       }
     end,
+  },
 
-    -- telescope
-    {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+  -- telescope
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-    -- treesitter
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function () 
-        local configs = require("nvim-treesitter.configs")
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-          ensure_installed = { "c", "json", "lua", "proto", "typescript", "go", "gomod", "gosum", "vim", "vimdoc", "regex", "sql" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end
-    },
+      configs.setup({
+        ensure_installed = { "c", "json", "lua", "proto", "typescript", "go", "gomod", "gosum", "vim", "vimdoc", "regex", "sql" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
 
-    -- neo-tree
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", 
-        "MunifTanjim/nui.nvim",
-      }
+  -- neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
     }
-  }
-}
+  },
 
-return {
-  spec,
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "habamax" } },
   install = { colorscheme = { "poimandres" } },
+    
   -- automatically check for plugin updates
   checker = { enabled = true },
 }
+
