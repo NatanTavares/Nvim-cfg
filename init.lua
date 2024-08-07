37d73be8e1750065465d512f851891fdf1ee5166
NatanTavares <nattantavares.s15@gmail.com>
2024-08-07 13:52:14 -0300

    Setup Neo-Tree, Lualine, Nui plugins, add vim-opts for consistency.

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -27,5 +21,6 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+require("vim-opts")
 require("lazy").setup("plugins")
 
