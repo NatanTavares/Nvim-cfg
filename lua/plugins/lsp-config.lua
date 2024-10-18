53d31449b87599ce62cd4c81932f847f08613141
NatanTavares <nattantavares.s15@gmail.com>
2024-10-18 13:52:54 -0300

    adds yaml lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,47 @@
-local lsps = { "lua_ls", "golangci_lint_ls", "gopls", "ts_ls" }
+local lsps = { "lua_ls", "golangci_lint_ls", "gopls", "bufls", "ts_ls", "yamlls" }
 
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			require("mason").setup({
-				-- ensure_installed = lsps,
-			})
-			require("mason-lspconfig").setup({
-				ensure_installed = lsps,
-				automatic_installation = true,
-			})
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      require("mason").setup({
+        ensure_installed = lsps,
+      })
+      require("mason-lspconfig").setup({
+        ensure_installed = lsps,
+        automatic_installation = true,
+      })
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.golangci_lint_ls.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.golangci_lint_ls.setup({ capabilities = capabilities })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set({ "n", "v" }, "<leader>.", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig.bufls.setup({ capabilities = capabilities })
+      lspconfig.yamlls.setup({ capabilities = capabilities })
+
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set({ "n", "v" }, "<leader>.", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
