0558469adddbb68ea814c08576bd34e6aad2ed69
NatanTavares <nattantavares.s15@gmail.com>
2024-10-30 11:54:31 -0300

    show dot-files at neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,5 +10,25 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>E", ":Neotree filesystem toggle<CR>", {})
+
+    require("neo-tree").setup({
+      close_if_last_window = true,
+
+      filesystem = {
+        hijack_netrw_behavior = "disabled",
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_by_name = {},
+          never_show = {
+            "node_modules",
+            ".idea",
+            ".DS_Store",
+          },
+        },
+      },
+    })
   end,
 }
